0051338cd5c04c77309862692e18be336e12c31f
Muhammet Arslan <m.arslan@DEAMWS4314-1223.local>
2019-06-23 12:57:57 +0200

    new aws builder

--- pkg/aws/aws.go
+++ pkg/aws/aws.go
@@ -1,17 +1,36 @@
 package aws
 
+import (
+
+	"html/template"
+)
+
 // Variables
 var (
 	debug bool
 
 	masterCount int
 	workerCount int
+
+	templates []
 )
 
-// Execute will run main logic
+
+// Build will run main logic
 func Build(debug bool, masterCount int, workerCount int) {
 
 	debug = debug
 	masterCount = masterCount
 	workerCount = workerCount
+
+	getTemplates()
 }
+
+func getTemplates() {
+
+	templates["main"], templates["error"] = template.ParseFiles("../template/aws/main.tf.kubeman")
+
+}
+
+
+
