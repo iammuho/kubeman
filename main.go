228989dfc2d03eb39a27e2dcff5b48a6fd00b340
Muhammet Arslan <m.arslan@deamws4314.intern>
2019-06-23 11:38:50 +0200

    new cmd builder

--- main.go
+++ main.go
@@ -16,46 +16,10 @@ package main
 import (
 	"flag"
 	"log"
+
+	"github.com/geass/kubeman/cmd"
 )
 
-// Variables
-var (
-	DefaultConfigPath = "terraform"
-)
-
-// Flag options from CLI
-type Options struct {
-    Verbose bool `short:"v" long:"verbose" description:"Show verbose debug information"`
-    Output string `short:"o" long:"output" description:"What is the output type?"`
-}
-
-// Controller struct
-type kubeManController struct {
-	Options Options
-}
-
-// MAIN Function that to be run on init
 func main() {
-
-	verbose := flag.Bool("v", true, "Show verbose debug information")
-	output := flag.String("o", "terraform", "Generate output as")
-
-	flag.Parse()
-
-	options := Options {
-		Verbose: *verbose,
-		Output: *output,
-	}
-
-	kubeMan := kubeManController {
-		Options: options,
-	}
-
-    kubeMan.Run()
-}
-
-// RUN function that to be triggered by main function for initial startup
-func (k *kubeManController) Run() {
-
-	log.Print("WKM app started")
+	cmd.Execute()
 }
